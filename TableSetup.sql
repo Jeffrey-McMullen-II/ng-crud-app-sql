8f76b9cd65c7b338105f657d0cdfc26d4ea704d4
Jeffrey <jeffrey.mcmullen@yahoo.com>
2020-07-04 13:05:15 -0400

    Create tables: util_log_types, logs, users

--- TableSetup.sql
+++ TableSetup.sql
@@ -1,13 +1,40 @@
-# BEGIN Setup crud_database.util_log_types
+DROP TABLE IF EXISTS crud_database.logs;
 DROP TABLE IF EXISTS crud_database.util_log_types;
+DROP TABLE IF EXISTS crud_database.users;
 
+-- BEGIN Setup crud_database.util_log_types
 CREATE TABLE crud_database.util_log_types (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    description VARCHAR(255) NOT NULL
+    id INT NOT NULL AUTO_INCREMENT,
+    description VARCHAR(255) NOT NULL,
+	PRIMARY KEY (id)
 );
 
 INSERT INTO crud_database.util_log_types (description)
 VALUES
 ('INFO'),
 ('ERROR');
-# END Setup crud_database.util_log_types
+-- END Setup crud_database.util_log_types
+
+
+
+-- BEGIN Setup crud_database.logs
+CREATE TABLE crud_database.logs (
+    id INT NOT NULL AUTO_INCREMENT,
+    log_type_id INT NOT NULL,
+    description TEXT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (log_type_id) REFERENCES crud_database.util_log_types(id)
+);
+-- END Setup crud_database.logs
+
+
+
+-- BEGIN Setup crud_database.users
+CREATE TABLE crud_database.users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(50),
+    first_name VARCHAR(25),
+	last_name VARCHAR(25),
+    PRIMARY KEY (id)
+);
+-- END Setup crud_database.logs
