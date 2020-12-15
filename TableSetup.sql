4a993b9183c2b7b92642044c5d1f949d13f7c261
unknown <jeffrey.mcmullen@yahoo.com>
2020-12-15 00:54:32 -0500

    Use more explicit naming

--- TableSetup.sql
+++ TableSetup.sql
@@ -1,17 +1,18 @@
 DROP TABLE IF EXISTS crud_database.logs;
 DROP TABLE IF EXISTS crud_database.util_log_types;
 DROP TABLE IF EXISTS crud_database.users;
+DROP TABLE IF EXISTS crud_database.files;
 
 
 
 -- BEGIN Setup crud_database.util_log_types
 CREATE TABLE crud_database.util_log_types (
-    id INT NOT NULL AUTO_INCREMENT,
-    description VARCHAR(255) NOT NULL,
-	PRIMARY KEY (id)
+    log_type_id INT NOT NULL AUTO_INCREMENT,
+    log_type_description VARCHAR(255) NOT NULL,
+	PRIMARY KEY (log_type_id)
 );
 
-INSERT INTO crud_database.util_log_types (description)
+INSERT INTO crud_database.util_log_types (log_type_description)
 VALUES
 ('DEBUG'),
 ('INFO'),
@@ -22,12 +23,12 @@ VALUES
 
 -- BEGIN Setup crud_database.logs
 CREATE TABLE crud_database.logs (
-    id INT NOT NULL AUTO_INCREMENT,
+    log_id INT NOT NULL AUTO_INCREMENT,
     log_type_id INT NOT NULL,
-    description TEXT NOT NULL,
+    log_message TEXT NOT NULL,
 	log_date DATETIME NOT NULL DEFAULT NOW(),
-    PRIMARY KEY (id),
-    FOREIGN KEY (log_type_id) REFERENCES crud_database.util_log_types(id)
+    PRIMARY KEY (log_id),
+    FOREIGN KEY (log_type_id) REFERENCES crud_database.util_log_types(log_type_id)
 );
 -- END Setup crud_database.logs
 
@@ -35,10 +36,22 @@ CREATE TABLE crud_database.logs (
 
 -- BEGIN Setup crud_database.users
 CREATE TABLE crud_database.users (
-    id INT NOT NULL AUTO_INCREMENT,
-    email VARCHAR(50),
-    first_name VARCHAR(25),
-	last_name VARCHAR(25),
-    PRIMARY KEY (id)
+    user_id INT NOT NULL AUTO_INCREMENT,
+    user_email VARCHAR(50),
+    user_first_name VARCHAR(25),
+	user_last_name VARCHAR(25),
+    PRIMARY KEY (user_id)
 );
--- END Setup crud_database.logs
+-- END Setup crud_database.users
+
+
+
+-- BEGIN Setup crud_database.files
+CREATE TABLE crud_database.files (
+    file_id INT NOT NULL AUTO_INCREMENT,
+    file_name VARCHAR(50),
+    file_type VARCHAR(50),
+	file_contents LONGBLOB,
+    PRIMARY KEY (file_id)
+);
+-- END Setup crud_database.files
