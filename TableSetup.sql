32ce33550b5f175cebe357ff32c5215a26cac3d9
Jeffrey <jeffrey.mcmullen@yahoo.com>
2021-06-01 09:55:28 -0400

    Update database name and add image type util table

--- TableSetup.sql
+++ TableSetup.sql
@@ -1,53 +1,69 @@
-DROP DATABASE IF EXISTS crud_database;
-CREATE DATABASE crud_database;
+DROP DATABASE IF EXISTS crud;
+CREATE DATABASE crud;
 
--- BEGIN Setup crud_database.util_log_types
-CREATE TABLE crud_database.util_log_types (
+-- BEGIN Setup crud.util_log_types
+CREATE TABLE crud.util_log_types (
     log_type_id INT NOT NULL AUTO_INCREMENT,
     log_type_description VARCHAR(255) NOT NULL,
 	PRIMARY KEY (log_type_id)
 );
 
-INSERT INTO crud_database.util_log_types (log_type_description)
+INSERT INTO crud.util_log_types (log_type_description)
 VALUES
 ('DEBUG'),
 ('INFO'),
 ('ERROR');
--- END Setup crud_database.util_log_types
+-- END Setup crud.util_log_types
 
 
 
--- BEGIN Setup crud_database.logs
-CREATE TABLE crud_database.logs (
+-- BEGIN Setup crud.logs
+CREATE TABLE crud.logs (
     log_id INT NOT NULL AUTO_INCREMENT,
     log_type_id INT NOT NULL,
     log_message TEXT NOT NULL,
 	log_date DATETIME NOT NULL DEFAULT NOW(),
     PRIMARY KEY (log_id),
-    FOREIGN KEY (log_type_id) REFERENCES crud_database.util_log_types(log_type_id)
+    FOREIGN KEY (log_type_id) REFERENCES crud.util_log_types(log_type_id)
 );
--- END Setup crud_database.logs
+-- END Setup crud.logs
 
 
 
--- BEGIN Setup crud_database.users
-CREATE TABLE crud_database.users (
+-- BEGIN Setup crud.users
+CREATE TABLE crud.users (
     user_id INT NOT NULL AUTO_INCREMENT,
     user_email VARCHAR(50),
     user_first_name VARCHAR(25),
 	user_last_name VARCHAR(25),
     PRIMARY KEY (user_id)
 );
--- END Setup crud_database.users
+-- END Setup crud.users
 
 
 
--- BEGIN Setup crud_database.files
-CREATE TABLE crud_database.files (
+-- BEGIN Setup crud.files
+CREATE TABLE crud.files (
     file_id INT NOT NULL AUTO_INCREMENT,
     file_name VARCHAR(50),
     file_type VARCHAR(50),
 	file_contents LONGBLOB,
     PRIMARY KEY (file_id)
 );
--- END Setup crud_database.files
+-- END Setup crud.files
+
+
+
+-- BEGIN Setup crud.util_image_types
+CREATE TABLE crud.util_image_types (
+    image_type_id INT NOT NULL AUTO_INCREMENT,
+    image_type VARCHAR(50),
+    PRIMARY KEY (image_type_id)
+);
+
+INSERT INTO crud.util_image_types (image_type)
+VALUES
+('image/gif'),
+('image/jpeg'),
+('image/png');
+-- END Setup crud.util_image_types
